03fbe3662ce67758256c445a32d1cd1db62f5aca
Luqman Malik <hakim19jan@gmail.com>
2023-11-28 21:41:45 +0800

    Added team formation section

--- kitahack-frontend/src/app/app.module.ts
+++ kitahack-frontend/src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { NgModule } from "@angular/core";
-import { DefaultLayoutComponent } from "./layouts/default-layout/default-layout.component";
-import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app.routes";
-import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { NgModule } from '@angular/core';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { TeamFormationSectionComponent } from './team-formation-section/team-formation-section.component';
 
 @NgModule({
-  declarations: [
-    DefaultLayoutComponent,
-    AppComponent,
-  ],
+  declarations: [DefaultLayoutComponent, AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HeroSectionComponent,
+    TeamFormationSectionComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
-}
+export class AppModule {}
---
+++ kitahack-frontend/src/app/team-formation-section/team-formation-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-team-formation-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './team-formation-section.component.html',
+  styleUrl: './team-formation-section.component.css'
+})
+export class TeamFormationSectionComponent {
+
+}
