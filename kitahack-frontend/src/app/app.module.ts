2e4f524aaeb909f9eef9f70aef825aaf7c81cc16
Kayin <34983642+kayinleong@users.noreply.github.com>
2023-12-01 01:04:55 +0800

    Refactored Codebase

--- kitahack-frontend/src/app/app.module.ts
+++ kitahack-frontend/src/app/app.module.ts
@@ -2,35 +2,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
-<<<<<<< HEAD
-import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { TeamFormationSectionComponent } from './team-formation-section/team-formation-section.component';
-import { DetailsComponent } from "./details/details.component";
-import { ChatboxComponent } from './chatbox/chatbox.component';
-import { JudgingCriteriaSectionComponent } from './judging-criteria-section/judging-criteria-section.component';
-import { MentorsSectionComponent } from './mentors-section/mentors-section.component';
-
-@NgModule({
-    declarations: [DefaultLayoutComponent, AppComponent],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HeroSectionComponent,
-        TeamFormationSectionComponent,
-        DetailsComponent,
-        ChatboxComponent,
-        JudgingCriteriaSectionComponent,
-        MentorsSectionComponent,
-    ]
-=======
 
 // Layouts
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
-
 
 @NgModule({
   declarations: [
@@ -41,10 +17,6 @@ import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.
   ],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
->>>>>>> f911b06653d6cc4b9b2e79df1cc70e6ee3374f72
+  imports: [BrowserModule, AppRoutingModule],
 })
 export class AppModule {}
--- kitahack-frontend/src/app/pages/home/home.module.ts
+++ kitahack-frontend/src/app/pages/home/home.module.ts
@@ -1,32 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home.routes';
 
 // Components
 import { HomeComponent } from './home.component';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { CountdownSectionComponent } from '../../components/countdown-section/countdown-section.component';
+import { DetailsComponent } from '../../components/details/details.component';
 import { WhatiskitahackSectionComponent } from '../../components/what-is-kitahack-section/what-is-kitahack-section.component';
 import { TeamFormationSectionComponent } from '../../components/team-formation-section/team-formation-section.component';
 import { FAQsSectionComponent } from '../../components/faqs-section/faqs-section.component';
-import { DetailsComponent } from '../../components/details/details.component';
 import { ChatboxComponent } from '../../components/chatbox/chatbox.component';
-import { HomeRoutingModule } from './home.routes';
-
+import { MentorComponent } from '../../components/mentor/mentor.component';
+import { MentorsSectionComponent } from '../../components/mentors-section/mentors-section.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeroSectionComponent,
     CountdownSectionComponent,
+    DetailsComponent,
     WhatiskitahackSectionComponent,
     TeamFormationSectionComponent,
-    DetailsComponent,
     ChatboxComponent,
+    MentorComponent,
+    MentorsSectionComponent,
     FAQsSectionComponent,
   ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
