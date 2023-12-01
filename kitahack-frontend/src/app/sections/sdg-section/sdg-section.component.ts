028834149e71391b279ff7c055159e8cd6b00b56
Kayin <34983642+kayinleong@users.noreply.github.com>
2023-12-01 23:40:17 +0800

    Added SDG Section; Overrall UI Improvements

---
+++ kitahack-frontend/src/app/sections/sdg-section/sdg-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sdg-section',
+  templateUrl: './sdg-section.component.html',
+  styleUrl: './sdg-section.component.css',
+})
+export class SdgSectionComponent {}
