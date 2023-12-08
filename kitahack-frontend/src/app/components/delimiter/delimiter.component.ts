f1415f265552d285e4415141299b11657e841d8e
Luqman Malik <hakim19jan@gmail.com>
2023-12-08 19:40:31 +0800

    Created delimiter component

---
+++ kitahack-frontend/src/app/components/delimiter/delimiter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-delimiter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './delimiter.component.html',
+  styleUrl: './delimiter.component.css'
+})
+export class DelimiterComponent {
+
+}
